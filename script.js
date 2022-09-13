fff900c49500881768862cfdc3866a066a5a1431
White-Hat-Designer <97239651+SK-Chaurasiya@users.noreply.github.com>
2022-09-13 14:34:02 +0530

    Files Updata
    by White Hat Designer

---
+++ script.js
@@ -0,0 +1,88 @@
+/* =====================================
+     change nav style on scroll
+   ===================================== */
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 100);
+});
+
+
+
+/* =====================================================
+         Contact buttons (circular text buttons)
+  ====================================================== */
+const textButtons = document.querySelectorAll(".contact__btn");
+
+textButtons.forEach(textButton => {
+  let text = textButton.querySelector("p");
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+      `<span style='transform: rotate(${index * 12}deg)'>${character}</span>`
+    ).join("");
+});
+
+
+
+/* ===============================
+          Initialize Swiper 
+   =============================== */
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1021: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    }
+  }
+});
+
+
+
+/* =========================================
+          Navigation on/off 
+   ========================================= */
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+}
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = 'inline-block';
+  closeNavBtn.style.display = 'none';
+}
+closeNavBtn.addEventListener('click', closeNav);
+
+// close nav menu on click of menu link/items
+if (document.body.clientWidth < 1024) {
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  })
+}
